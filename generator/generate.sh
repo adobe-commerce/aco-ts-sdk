d65fdd19d5ae2c3ce613c186f62512d25af1cf3a
Ross Brandon <31011626+rossbrandon@users.noreply.github.com>
2025-06-25 17:39:41 -0500

    Add missing contribution and code of conduct files as well as some license headers for open source requirements (#20)

--- generator/generate.sh
+++ generator/generate.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# ============================================================================
+# Copyright 2025 Adobe. All Rights Reserved.
+#
+# This file is licensed to you under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License. You may obtain a copy
+# of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+# OF ANY KIND, either express or implied. See the License for the specific language
+# governing permissions and limitations under the License.
+# ============================================================================
+
 FORCE=false
 GEN_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SPEC_FILE_URL="https://raw.githubusercontent.com/AdobeDocs/commerce-services/refs/heads/ccdm-early-access/src/openapi/data-ingestion-schema-v1.yaml"
